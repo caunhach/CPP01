bf26393eaed296ecd93bb5f4f8ef6822fe042c9e
Chayapon Aunhachoke <caunhach@z1t3c1.42bangkok.com>
2023-11-19 16:24:04 +0700

    last updated 19/11/2023

---
+++ ex03/Weapon.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caunhach <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/19 16:10:27 by caunhach          #+#    #+#             */
+/*   Updated: 2023/11/19 16:10:29 by caunhach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_H
+# define WEAPON_H
+
+#include <string>
+
+class Weapon {
+	private:
+		std::string	_type;
+	public:
+		std::string	&getType(void) const;
+		void		setType(std::string type);
+};
+
+#endif
